a396b328c5f0a5803ff7b8af315e7e49eda42aad
Jac0b2000 <rahuljacobsunil@gmail.com>
2023-09-06 22:35:48 +0530

    second commit

--- PersonalDetails.java
+++ PersonalDetails.java
@@ -3,8 +3,8 @@ public class PersonalDetails
     public static void main(String[] args)
     {
         System.out.println("Name:Rahul Jacob");
-        System.out.println("Address:Hno:19,\n\tSreevilas lane,\n\tKowdiar.P.O");
-        System.out.println("Pincode:695003");
+        System.out.println("Address:Hno:19,\n\tSreevilas lane,\n\tkowdiar.P.O");
         System.out.println("blood group:O+ve");
+        System.out.println("college:");
     }
 }
