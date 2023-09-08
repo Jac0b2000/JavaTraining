184a08b1fd184cf6c98afc836e7c384fa0e8eb69
Jac0b2000 <rahuljacobsunil@gmail.com>
2023-09-08 11:21:17 +0530

    Third commit

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes
+{
+    public static void main(String[] args)
+    {
+      int a=1000;
+      int b=1545;
+      long c=a*b;
+      double d=3.123;
+      double e=c/d;
+      char t='a';
+      System.out.println(a);
+      System.out.println(b);
+      System.out.print("a*b=");
+      System.out.println(c);
+      System.out.println(d);
+      System.out.print("c/d=");
+      System.out.println(e);
+      System.out.print(t);
+      System.out.println(" is a character");  
+    }
+}
