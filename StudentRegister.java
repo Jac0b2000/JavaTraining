2474c5be684c306f00861accd4f4229935f7d70a
Jac0b2000 <rahuljacobsunil@gmail.com>
2023-09-11 14:30:10 +0530

    fifth  commit

---
+++ StudentRegister.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class StudentRegister
+{
+    public static void main(String[] args)
+    {
+        Scanner s= new Scanner(System.in);
+        Student a= new Student();
+        System.out.println("Enter student name");
+        a.name= s.nextLine();
+        System.out.println("Enter student age");
+        a.age= s.nextInt();
+        System.out.println("Enter student RegNum");
+        a.regNum= s.nextLine();
+        
+        System.out.println("Name:-" + a.name);
+        System.out.println("Age:-" + a.age);
+        System.out.println("RegNum:-" +a.regNum);
+
+        s.close();
+    }
+
+}
